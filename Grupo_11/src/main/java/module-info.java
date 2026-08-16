6a39dac997dd36d9831289407840080e34c373c5
Fernando Zambrano <fezaguer@espol.edu.ec>
2026-08-16 16:17:33 -0500

    permisos

--- Grupo_11/src/main/java/module-info.java
+++ Grupo_11/src/main/java/module-info.java
@@ -4,4 +4,10 @@ module com.rafaelcosmo.grupo_11 {
 
     opens com.rafaelcosmo.grupo_11 to javafx.fxml;
     exports com.rafaelcosmo.grupo_11;
+
+    // Permisos para que JavaFX lea nuestra arquitectura
+    opens com.rafaelcosmo.grupo_11.vista to javafx.fxml;
+    exports com.rafaelcosmo.grupo_11.vista;
+    exports com.rafaelcosmo.grupo_11.modelo;
+    exports com.rafaelcosmo.grupo_11.minimax;
 }
